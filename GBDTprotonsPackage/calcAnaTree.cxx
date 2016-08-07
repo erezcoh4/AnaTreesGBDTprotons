326a7289d8773a6c6e3bdf11ab1a079132f08aea
Erez Cohen <erezcohen@erezs-mbp.guest-network.net>
2016-08-07 01:46:01 -0500

    Sun Aug  7 01:46:01 CDT 2016

--- GBDTprotonsPackage/calcAnaTree.cxx
+++ GBDTprotonsPackage/calcAnaTree.cxx
@@ -300,7 +300,7 @@ void calcAnaTree::LoopPanNuTracks(){
             if(debug>3) Printf("plugging also MC information:");
             bool FoundMCtrack = false;
             for(Int_t ig4=0; ig4 < geant_list_size; ig4++) {
-                if(debug>3) Printf("truth_pdg[%d] = %d",ig4,truth_pdg[ig4]);
+                if(debug>3) Printf("cTrack.track_id = %d, trkg4id_pandoraNu[%d] = %d, truth_pdg[%d] = %d",cTrack.track_id,ig4,trkg4id_pandoraNu[ig4],ig4,truth_pdg[ig4]);
                 if(trkg4id_pandoraNu[ig4] == cTrack.track_id){
                     // lets start with only the MC pdg code, for training purposes
                     if(debug>3) Printf("truth pdg is: %d",truth_pdg[ig4]);
